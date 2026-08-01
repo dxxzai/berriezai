a9decec775440367708189077179818a9005acb2
dxxzai <dxxzaiamvs@gmail.com>
2026-07-31 20:12:18 -0400

    nav + subscribe

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,21 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+export async function GET(context) {
+  const chapters = await getCollection("chapters");
+
+  return rss({
+    title: "I'm a Young God, Won't You Raise Me?",
+    description: "New Chapter Updates",
+    site: context.site,
+
+    items: chapters
+      .sort((a, b) => b.data.chapter - a.data.chapter)
+      .map((chapter) => ({
+        title: `Chapter ${chapter.data.chapter}`,
+        pubDate: new Date(),
+        description: "A new chapter has been released!",
+        link: `berriezai/chapters/${chapter.id}/`,
+      })),
+  });
+}
